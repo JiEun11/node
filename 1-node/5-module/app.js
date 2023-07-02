eb5b4d5a22f96e1310c792c6ba341b75dd9c8437
Bella <shdudtnr3939@naver.com>
2023-07-02 15:51:24 +0900

    [add]JS에서 제공하는 module 사용

---
+++ 1-node/5-module/app.js
@@ -0,0 +1,8 @@
+import { increase, getCount } from "./counter.js";
+// import * as counter from "./counter.js";
+
+// counter.increase();  로도 사용 가능
+increase();
+increase();
+increase();
+console.log(getCount());
