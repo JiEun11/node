cfbc466e623d710ec702e66519062cd1a6b52bcf
Bella <shdudtnr3939@naver.com>
2023-07-10 22:36:43 +0900

    [add] NODE로 서버 html 보내기

--- 9-node-server/1-http.js
+++ 9-node-server/1-http.js
@@ -10,7 +10,26 @@ const server = http.createServer((req, res)=> {
     console.log(req.httpVersion);
     console.log(req.method);
     console.log(req.url);
-    res.write('Welcome!');
+    const url = req.url;
+    if(url === '/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Hello</title></head>');
+        res.write('<body><h1>Welcome!</h1></h1></body>')
+        res.write('</html>');
+    } else if(url === '/courses'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Hello</title></head>');
+        res.write('<body><h1>Course</h1></h1></body>')
+        res.write('</html>');
+    }else{
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Academy</title></head>');
+        res.write('<body><h1>NOT FOUND!</h1></h1></body>')
+        res.write('</html>');
+    }
     res.end();
 });
 
