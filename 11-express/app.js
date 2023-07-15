2e79acf63bdacade9c7bb98ec1688fdbb46760e5
Bella <shdudtnr3939@naver.com>
2023-07-15 21:55:20 +0900

    [modified] Router 101

--- 11-express/app.js
+++ 11-express/app.js
@@ -7,6 +7,15 @@ const app = express();
 
 app.use(express.json());
 
+app
+  .route("/posts")
+  .get((req, res, next) => {
+    res.status(201).send("GET: /posts");
+  })
+  .post((req, res) => {
+    res.status(201).send("POST: /posts");
+  });
+
 app.get("/posts", (req, res) => {
   res.status(201).send("GET: /posts");
 });
