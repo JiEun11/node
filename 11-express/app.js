0cc4f6a47fd4bf6801d70fe8fd9a65140c55235b
Bella <shdudtnr3939@naver.com>
2023-07-15 16:20:25 +0900

    [add] 에러 처리의 모든 것(동기, 비동기)

--- 11-express/app.js
+++ 11-express/app.js
@@ -1,9 +1,43 @@
 import express from "express";
+import fs from "fs";
+import fsAsync from "fs/promises";
+
 const app = express();
 
 app.use(express.json());
 
-app.post("/", (req, res, next) => {
-  console.log(req.body);
+app.get("/file", (req, res) => {});
+
+app.get("/file1", (req, res) => {
+  try {
+    const data = fs.readFileSync("/file1.txt");
+    res.send(data);
+  } catch (error) {
+    res.sendStatus(404);
+  }
 });
+
+app.get("/file2", (req, res) => {
+  fsAsync
+    .readFile("/file2.txt")
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => res.sendStatus(404));
+});
+
+app.get("/file3", async (req, res) => {
+  try {
+    const data = await fsAsync.readFile("/file2.txt");
+    res.send(data);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+});
+
+app.use((error) => {
+  console.error(error);
+  res.status(500).json({ message: "Something went wrong" });
+});
+
 app.listen(8080);
