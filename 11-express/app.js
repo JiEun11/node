16f28ff2b884ef8880d8fa69e36db7190650dfa4
Bella <shdudtnr3939@naver.com>
2023-07-15 16:49:17 +0900

    [add] 비동기 에러 처리하는 방법 - 최신 버전

--- 11-express/app.js
+++ 11-express/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fs from "fs";
 import fsAsync from "fs/promises";
+import "express-async-errors";
 
 const app = express();
 
@@ -17,22 +18,15 @@ app.get("/file1", (req, res) => {
   }
 });
 
-app.get("/file2", (req, res) => {
-  fsAsync
-    .readFile("/file2.txt")
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((error) => res.sendStatus(404));
+app.get("/file2", async (req, res) => {
+  return fsAsync.readFile("/file2.txt").then((data) => {
+    res.send(data);
+  });
 });
 
-app.get("/file3", async (req, res) => {
-  try {
-    const data = await fsAsync.readFile("/file2.txt");
-    res.send(data);
-  } catch (error) {
-    res.sendStatus(404);
-  }
+app.get("/file3", async function (req, res) {
+  const data = await fsAsync.readFile("/file2.txt");
+  res.send(data);
 });
 
 app.use((error) => {
