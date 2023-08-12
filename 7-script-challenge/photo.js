b2d0646d998519f315755fa871dd8ce83abe1efe
Bella <shdudtnr3939@naver.com>
2023-08-12 21:32:36 +0900

    [add] 보정 시 생긴 파일이 있으면 원본 파일 duplicated 폴더로 옮기기

--- 7-script-challenge/photo.js
+++ 7-script-challenge/photo.js
@@ -27,17 +27,20 @@ const doCheckFileExt = (element) => {
     // 동영상 파일인지 체크
     if (extName === ".mp4" || extName === ".mov") {
       isDirExist(videoDirPath);
-      doMoveFile(videoDirPath, file);
+      doMoveFile(videoDirPath, file.base);
     }
     // 스크린샷 or iphone에서 편집된 사진 파일인지 체크
     else if (extName === ".png" || extName === ".aae") {
       isDirExist(capturedDirPath);
-      doMoveFile(capturedDirPath, file);
+      doMoveFile(capturedDirPath, file.base);
     }
 
     // iphone에서 사진 보정 시 생기는 파일인지 체크
     else if (imgFileName.base.includes("IMG_E")) {
       console.log(imgFileName);
+      const originalImgFileBase = file.name.replace("IMG_E", "IMG_");
+      isDirExist(duplicatedDirPath);
+      doMoveFile(duplicatedDirPath, originalImgFileBase);
     }
   });
 };
@@ -50,9 +53,9 @@ const isDirExist = (dirPath) => {
 };
 
 // 해당 폴더로 옮기기
-const doMoveFile = (dirPath, file) => {
+const doMoveFile = (dirPath, fileBase) => {
   fsSync.renameSync(
-    targetDirPath + path.sep + file.name,
-    dirPath + path.sep + file.name
+    targetDirPath + path.sep + fileBase,
+    dirPath + path.sep + fileBase
   );
 };
