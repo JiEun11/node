13baf61786db72223eab5b6c4abcd0bcaf7eeede
Bella <shdudtnr3939@naver.com>
2023-08-13 20:44:57 +0900

    [엘리] 폴더 안에 각 폴더 생성하기

--- 7-script-challenge/app.js
+++ 7-script-challenge/app.js
@@ -13,5 +13,12 @@ if (!folder || !fs.existsSync(workingDir)) {
 }
 
 // 2. 그 폴더 안에 video, captured, duplicated 폴더를 만든다.
+const videoDir = path.join(workingDir, "video");
+const capturedDir = path.join(workingDir, "caputred");
+const duplicatedDir = path.join(workingDir, "duplicated");
+
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+!fs.existsSync(duplicatedDir) && s.mkdirSync(duplicatedDir);
 
 // 3. 폴더 안에 있는 파일들을 다 돌면서 해당하는 mp4/mov, png/aae, IMG_1234 (IMG_E1234)
