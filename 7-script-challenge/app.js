6645385e2a4117e1e0b5a8c08ebff15ad7c87041
Bella <shdudtnr3939@naver.com>
2023-08-13 21:23:46 +0900

    [엘리] 폴더 안에 있는 파일들 돌기

--- 7-script-challenge/app.js
+++ 7-script-challenge/app.js
@@ -19,6 +19,31 @@ const duplicatedDir = path.join(workingDir, "duplicated");
 
 !fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
 !fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
-!fs.existsSync(duplicatedDir) && s.mkdirSync(duplicatedDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
 
 // 3. 폴더 안에 있는 파일들을 다 돌면서 해당하는 mp4/mov, png/aae, IMG_1234 (IMG_E1234)
+fs.promises.readdir(workingDir).then(processFiles).catch(console.log);
+
+function processFiles(files) {
+  files.forEach((file) => {
+    if (isVideoFile(file)) {
+      console.log("video", file);
+    } else if (isCapturedFile(file)) {
+      console.log("captured", file);
+    } else if (isDuplicatedFile(file)) {
+      console.log("duplicated", file);
+    }
+  });
+}
+
+function isVideoFile(file) {
+  return true;
+}
+
+function isCapturedFile(file) {
+  return true;
+}
+
+function isDuplicatedFile(file) {
+  return true;
+}
