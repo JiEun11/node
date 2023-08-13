691a8e18f0b2f9561ea7474741b043135a932ab2
Bella <shdudtnr3939@naver.com>
2023-08-13 22:17:19 +0900

    [엘리] duplicated 체크 및 파일 옮기기 구현 완료

--- 7-script-challenge/app.js
+++ 7-script-challenge/app.js
@@ -6,9 +6,9 @@ const os = require("os");
 
 const folder = process.argv[2];
 // const workingDir = path.join(os.homedir(), "Pictures", folder);
-const workingDir = path.parse(__filename).dir;
+const workingDir = path.join(path.parse(__filename).dir, folder);
 if (!folder || !fs.existsSync(workingDir)) {
-  console.error("Please enter folder name in Pictures");
+  console.error(`Please enter folder name in ${workingDir}`);
   return;
 }
 
@@ -22,28 +22,47 @@ const duplicatedDir = path.join(workingDir, "duplicated");
 !fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
 
 // 3. 폴더 안에 있는 파일들을 다 돌면서 해당하는 mp4/mov, png/aae, IMG_1234 (IMG_E1234)
+// (files) => processFiles(files) 인데 인자가 같으면 processFiles로 생략 가능
 fs.promises.readdir(workingDir).then(processFiles).catch(console.log);
 
 function processFiles(files) {
   files.forEach((file) => {
     if (isVideoFile(file)) {
-      console.log("video", file);
+      move(file, videoDir);
     } else if (isCapturedFile(file)) {
-      console.log("captured", file);
-    } else if (isDuplicatedFile(file)) {
-      console.log("duplicated", file);
+      move(file, capturedDir);
+    } else if (isDuplicatedFile(files, file)) {
+      move(file, duplicatedDir);
     }
   });
 }
 
 function isVideoFile(file) {
-  return true;
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  return !!match;
 }
 
 function isCapturedFile(file) {
-  return true;
+  const regExp = /(png|aae)$/gm;
+  const match = file.match(regExp);
+  return !!match;
 }
 
-function isDuplicatedFile(file) {
-  return true;
+function isDuplicatedFile(files, file) {
+  // IMG_XXXX -> IMG_EXXXX
+  if (!file.startsWith("IMG_") || file.startsWith("IMG_E")) {
+    return false;
+  }
+
+  const edited = `IMG_E${file.split("_")[1]}`;
+  const found = files.find((f) => f.includes(edited));
+  return !!found;
+}
+
+function move(file, targetDir) {
+  console.info(`move ${file} to ${path.basename(targetDir)}`);
+  const oldPath = path.join(workingDir, file);
+  const newPath = path.join(targetDir, file);
+  fs.promises.rename(oldPath, newPath).catch(console.error);
 }
