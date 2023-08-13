14a4f84592cd2fac11b8bf2f82d3d063f5dab75f
Bella <shdudtnr3939@naver.com>
2023-08-13 20:42:18 +0900

    [엘리] 1. 사용자가 입력한 폴더 이름 받아오기 및 예외처리

---
+++ 7-script-challenge/app.js
@@ -0,0 +1,17 @@
+// Plan
+// 1. 사용자가 원하는 폴더의 이름을 받아온다.
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+
+const folder = process.argv[2];
+// const workingDir = path.join(os.homedir(), "Pictures", folder);
+const workingDir = path.parse(__filename).dir;
+if (!folder || !fs.existsSync(workingDir)) {
+  console.error("Please enter folder name in Pictures");
+  return;
+}
+
+// 2. 그 폴더 안에 video, captured, duplicated 폴더를 만든다.
+
+// 3. 폴더 안에 있는 파일들을 다 돌면서 해당하는 mp4/mov, png/aae, IMG_1234 (IMG_E1234)
